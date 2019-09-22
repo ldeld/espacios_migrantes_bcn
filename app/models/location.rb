6fefece21be7799588d696fa7bc679c71c21091d
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2019-09-22 18:03:30 +0200

    dotenv and development gems

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ApplicationRecord
   validates :address, :name, presence: true
   validates :phone_number, format: /\A(\+34|0034|34)?[\s|\-|\.]?[6|7|8|9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}\z/
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
