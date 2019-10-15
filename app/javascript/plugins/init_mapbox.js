149b57a5d48fb4839a392267a4de26204a916262
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2019-10-15 19:56:30 +0200

    home page

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,32 @@
+import mapboxgl from 'mapbox-gl';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+};
+
+const drawMarkers = (map) => {
+  const markers = JSON.parse(map.dataset.markers);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+  });
+  return(markers);
+}
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/ldeld/ck0tp8bit0yyq1co683830r1l'
+    });
+    const markers = drawMarkers(mapElement);
+    fitMapToMarkers(map, markers);
+  }
+};
+
+export { initMapbox };
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'home', to: 'pages#home'
   get 'locations/index'
   get 'locations/map'
   get 'locations/show'
+
+  root to: 'pages#home'
 end
